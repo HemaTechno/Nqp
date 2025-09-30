23dcbefbbc554ac7e067a560eaf732a171ee378d
HemaTechno <ibrahimmansour4655@gmail.com>
2025-09-30 19:30:49 +0300

    Update and rename appwriteClient.js to firebaseConfig.js

--- appwriteClient.js
+++
@@ -1,9 +0,0 @@
-// appwriteClient.js
-import { Client, Account } from "https://esm.sh/appwrite";
-
-// حط هنا بيانات مشروعك
-const client = new Client()
-    .setEndpoint("https://fra.cloud.appwrite.io/v1") // Endpoint
-    .setProject("68dbf92f0021e60a075f"); // Project ID
-
-export const account = new Account(client);
---
+++ firebaseConfig.js
@@ -0,0 +1,18 @@
+// appwriteClient.js
+// firebaseConfig.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+// حط هنا بيانات مشروعك من Firebase Console
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// ✅ Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
